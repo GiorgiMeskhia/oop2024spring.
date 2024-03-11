3658616421d9df7f82b3e422c14fe2e98f0ab3fd
GiorgiMeskhia <162803856+GiorgiMeskhia@users.noreply.github.com>
2024-03-11 11:53:41 +0400

    Add files via upload

---
+++ Assignment03.java
@@ -0,0 +1,6 @@
+public class Assignment03 {
+    public static void main(String[] args) {
+        // Print the path to hidden.txt
+        System.out.println("Path to hidden.txt: quizes/q1/giorgi_meskhia_1/bird/cloud/book/hidden.txt");
+    }
+}
